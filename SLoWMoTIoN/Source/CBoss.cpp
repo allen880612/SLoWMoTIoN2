4e6479484880dd816d9688e1b42c5bd44852cafa
Raven9527 <acd667y@gmail.com>
2019-04-19 03:18:49 +0800

    SLoWMoTIoN Ver 1.10.8.5 have many problem (fuck==)

--- SLoWMoTIoN/Source/CBoss.cpp
+++ SLoWMoTIoN/Source/CBoss.cpp
@@ -11,10 +11,12 @@
 #include "CManager.h"
 #include "CLibrary.h"
 #include "CBoss.h"
+#include <vector>
 using namespace myLibrary;
 
 namespace game_framework
 {
+	#pragma region - CBoss -
 	CBoss::CBoss()
 	{
 		initHp = 0;
@@ -29,6 +31,8 @@ namespace game_framework
 		initHp = _hp;
 		loadPath = _loadPath;
 		transparentColor = color;
+
+		//Initialize();
 	}
 
 	CBoss::~CBoss()
@@ -52,12 +56,12 @@ namespace game_framework
 
 	void CBoss::Initialize()
 	{
-		#pragma region - 僅一次的載入圖片 -
+	#pragma region - 僅一次的載入圖片 -
 		if (animation.IsNull())
 		{
 			LoadBitmap();
 		}
-		#pragma endregion
+	#pragma endregion
 
 		inity = SIZE_Y - animation.Height();
 		SetCurrentXY(initx, inity);
@@ -107,4 +111,53 @@ namespace game_framework
 	{
 		return &animation;
 	}
+	#pragma endregion
+
+	#pragma region - CXingting -
+	CXingting::CXingting()
+	{
+		CBoss();
+	}
+
+	CXingting::CXingting(int _x, int _y, int _hp, BitmapPath _loadPath, COLORREF color)
+	{
+		CBoss(_x, _y, _hp, _loadPath, color); //constructor
+		Initialize();
+	}
+
+	CXingting::~CXingting()
+	{
+
+	}
+
+	void CXingting::Initialize()
+	{
+		CBoss::Initialize();
+
+		/*#pragma region - delete bullet -
+		for (vector<CScallion*>::iterator level4iter = level4.begin(); level4iter != level4.end(); level4iter++)
+		{
+		delete *level4iter;
+		}
+		level4.clear();
+		#pragma endregion*/
+
+		shootLevel4_cd = CTimer(1.0);
+	}
+
+	void CXingting::Attack1()
+	{
+		//shootLevel4_cd.CountDown();
+		//if (shootLevel4_cd.IsTimeOut())
+		//{
+		//	for (int i = 0; i < 3; i++)
+		//	{
+		//		CScallion *newlevel4 = new CScallion("Role\\books", "book", 4, 450, 360, 200 + i * 50, 360 + i * 50); //先創建一個蔥的物件
+		//		level4.push_back(newlevel4); //將蔥放進vector
+		//		shootLevel4_cd.ResetTime();
+		//	}
+		//}
+	}
+	#pragma endregion
+
 }
--- SLoWMoTIoN/Source/CBoss.h
+++ SLoWMoTIoN/Source/CBoss.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "CLibrary.h"
 #include "Refactor.h"
+#include <vector>
+#include "CBall.h"
 using namespace myLibrary;
 namespace game_framework
 {
+	#pragma region - CBoss -
 	class CBoss
 	{
 	public:
@@ -21,23 +24,43 @@ namespace game_framework
 		void OnMove();
 		void MoveWithMap(string);
 
+
+		virtual void Attack1() { };
+
 		CLayer layer;
-		
+
 		CAnimate* GetAnimate();
 
 	protected:
-		#pragma region Init Information
+	#pragma region Init boss Information
 		int initx, inity, initHp;
 		BitmapPath loadPath;
 		COLORREF transparentColor;
-		#pragma endregion
-		
+	#pragma endregion
+
 		CAnimate animation;
-		
+
 		int hp;
 		int currentX, currentY;
 		int x, y;
 		int width, height;
 		bool IsAlive = true;
+
 	};
+	#pragma endregion
+
+	#pragma region - CXingting -
+	class CXingting : public CBoss
+	{
+	public:
+		CXingting();
+		CXingting(int, int, int, BitmapPath, COLORREF);
+		~CXingting();
+		void Initialize();
+		void Attack1();
+	private:
+		vector<CScallion*>	level4;
+		CTimer shootLevel4_cd;
+	};
+	#pragma endregion
 }
