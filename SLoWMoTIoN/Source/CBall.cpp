6dd40bc74ec70f1536c63d6edbf27f70447355df
Raven9527 <acd667y@gmail.com>
2019-06-11 16:43:38 +0800

    SLoWMoTIoN Ver 1.18.9 : fuck ball

--- SLoWMoTIoN/Source/CBall.cpp
+++ SLoWMoTIoN/Source/CBall.cpp
@@ -22,63 +22,11 @@ namespace game_framework {
 	CBall::CBall()
 	{
 		is_alive = true;
-		x = y = dx = dy = index = delay_counter = 0;
 	}
-
-	//bool CBall::HitEraser(CEraser *eraser)
-	//{
-	//	// 檢測擦子所構成的矩形是否碰到球
-	//	return HitRectangle(eraser->GetX1(), eraser->GetY1(),
-	//		eraser->GetX2(), eraser->GetY2());
-	//}
-
-	//bool CBall::HitRectangle(int tx1, int ty1, int tx2, int ty2)
-	//{
-	//	int x1 = x + dx;				// 球的左上角x座標
-	//	int y1 = y + dy;				// 球的左上角y座標
-	//	int x2 = x1 + bmp.Width();	// 球的右下角x座標
-	//	int y2 = y1 + bmp.Height();	// 球的右下角y座標
-	//								//
-	//								// 檢測球的矩形與參數矩形是否有交集
-	//								//
-	//	return (tx2 >= x1 && tx1 <= x2 && ty2 >= y1 && ty1 <= y2);
-	//}
 
 	bool CBall::IsAlive()
 	{
 		return is_alive;
-	}
-
-	void CBall::LoadBitmap()
-	{
-		//bmp.LoadBitmap(IDB_BALL, RGB(0, 0, 0));			// 載入球的圖形
-		//bmp_center.LoadBitmap(IDB_CENTER, RGB(0, 0, 0));	// 載入球圓心的圖形
-	}
-
-	void CBall::OnMove()
-	{
-		if (!is_alive)
-			return;
-		delay_counter--;
-		if (delay_counter < 0) {
-			delay_counter = delay;
-			//
-			// 計算球向對於圓心的位移量dx, dy
-			//
-			const int STEPS = 18;
-			static const int DIFFX[] = { 35, 32, 26, 17, 6, -6, -17, -26, -32, -34, -32, -26, -17, -6, 6, 17, 26, 32, };
-			static const int DIFFY[] = { 0, 11, 22, 30, 34, 34, 30, 22, 11, 0, -11, -22, -30, -34, -34, -30, -22, -11, };
-			index++;
-			if (index >= STEPS)
-				index = 0;
-			dx = DIFFX[index];
-			dy = DIFFY[index];
-		}
-	}
-
-	void CBall::SetDelay(int d)
-	{
-		delay = d;
 	}
 
 	void CBall::SetIsAlive(bool alive)
@@ -93,15 +41,6 @@ namespace game_framework {
 			animation.SetTopLeft(x, y);
 	}
 
-	void CBall::OnShow()
-	{
-		/*if (is_alive) {
-		bmp.SetTopLeft(x + dx, y + dy);
-		bmp.ShowBitmap();
-		bmp_center.SetTopLeft(x, y);
-		bmp_center.ShowBitmap();
-		}*/
-	}
 	void CBall::SetCurrentXY(double cx, double cy)
 	{
 		currentX = cx;
@@ -115,12 +54,6 @@ namespace game_framework {
 
 	void CBall::LoadBitmap(string ziliaojia, string name, int number)
 	{
-		/*for (int i = 0; i < number; i++)
-		{
-			char *address = ConvertCharPointToString(ziliaojia, name, i);
-			animation.AddBitmap(address, RGB(255, 255, 255));
-			delete address;
-		}*/
 		animation.LoadBitmap(ziliaojia, name, number);
 	}
 
@@ -128,7 +61,6 @@ namespace game_framework {
 	{
 		animation.LoadBitmap(loadpath.ziliaojia, loadpath.name, loadpath.number, loadpath.color);
 	}
-
 
 	CAnimate * CBall::GetAnimate()
 	{
@@ -140,8 +72,6 @@ namespace game_framework {
 	#pragma region - CScallion -
 	CScallion::CScallion()
 	{
-		dx = dy = index = delay_counter = 0;	//暫時無用，只是確保繼承不會出問題
-
 		const int INIT_X = 0, INIT_Y = 0;
 		const int GRAVITY = 4;
 		currentX = INIT_X;
--- SLoWMoTIoN/Source/CBall.h
+++ SLoWMoTIoN/Source/CBall.h
@@ -14,16 +14,11 @@ namespace game_framework {
 	{
 	public:
 		CBall();
-		//bool HitEraser(CEraser *eraser);						// 是否碰到擦子
 		bool IsAlive();											// 是否活著
-		void LoadBitmap();										// 載入圖形
 
-		void OnMove();											// 移動
-		void OnShow();											// 將圖形貼到畫面
 		void SetCurrentXY(double, double);
 		void SetXY(int nx, int ny);								// 設定圓心的座標
 		void SetIsAlive(bool alive);							// 設定是否活著
-		void SetDelay(int d);									// 設定旋轉的速度
 		CPoint GetCenterPoint() { return CPoint(x + animation.Width() / 2, y + animation.Height() / 2); };
 
 		void LoadBitmap(string, string, int);
@@ -34,10 +29,6 @@ namespace game_framework {
 		CAnimate animation;
 		double currentX, currentY;     // /*當前的座標*/ //
 		int x, y;					// 圓心的座標
-		int dx, dy;					// 球距離圓心的位移量
-		int index;					// 球的「角度」，0-17表示0-360度
-		int delay_counter;			// 調整旋轉速度的計數器
-		int delay;					// 旋轉的速度
 		bool is_alive;				// 是否活著
 	private:
 		//bool HitRectangle(int tx1, int ty1, int tx2, int ty2);	// 是否碰到參數範圍的矩形
